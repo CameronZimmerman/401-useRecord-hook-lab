6a27e0594b3bfe88c49a0f13a63551e00554a6c9
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-05-31 12:24:53 -0700

    createuseRecord hook

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 const useRecord = (init) => {
+  const [current, setCurrent] = useState(init);
+  const [actions, setActions] = useState([init]);
+  const [count, setCount] = useState(0);
+
+  const undo = () => {
+    if(count <= 0) return;
+    setActions((prevActions) => {
+      const arr = [prevActions[prevActions.length - 1], ...prevActions];
+      arr.pop();
+      setCurrent(arr[arr.length - 1]);
+      setCount(oldCount => oldCount - 1);
+      return arr;
+    });
+  };
+
+  const redo = () => {
+    if(count >= actions.length - 1) return;
+    setActions((prevActions) => {
+      const arr = [...prevActions, prevActions[0]];
+      arr.shift();
+      setCurrent(arr[arr.length - 1]);
+      setCount(oldCount => oldCount + 1);
+      return arr;
+
+    });
+  };
+
+  const record = (color) => {
+    setActions((prevActions) => [...prevActions, color]);
+    setCurrent(color);
+    setCount(oldCount => oldCount + 1);
+  };
   
+  return {
+    current,
+    undo,
+    redo,
+    record
+  };
 };
 
 function App() {
@@ -10,10 +49,14 @@ function App() {
     <>
       <button onClick={undo}>undo</button>
       <button onClick={redo}>redo</button>
-      <input type="color" value={current} onChange={({ target }) => record(target.value)} />
-      <div style={{}}></div>
+      <input type="color"
+        value={current} 
+        onChange={({ target }) => record(target.value)} />
+      <div style=
+        {{ backgroundColor: current, width: '250px', height: '250px' }}>
+      </div>
     </>
-  )
+  );
 }
 
 export default App;
